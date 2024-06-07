3ed7740c6e75f59808505c5175b3c7f63dfae786
mdiannibelli <mdiannibelli@gmail.com>
2024-06-07 11:24:07 -0300

    usecallback & usememo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react"
 import Movies from "./components/Movies"
 import { useGetMovies } from "./hooks/getMovies"
 import { useSearchMovies } from "./hooks/searchMovies"
 function App() {
+  const [sort, setSort] = useState(false)
   const { search, setSearch, error } = useSearchMovies()
-  const { responseMovies, getMovies, loading } = useGetMovies({search})
+  const { responseMovies, getMovies, loading } = useGetMovies({search, sort})
 
   //const inputRef = useRef<HTMLInputElement>(null)
   const handleSubmit = (e:React.FormEvent) => {
@@ -11,7 +13,7 @@ function App() {
     //! Forma NO Controlada, usamos el DOM....
     //const value = inputRef.current?.value
     //console.log({value})
-    getMovies()
+    getMovies(search)
   }
 
   // Forma controlada, desventaja: se renderiza cada vez que cambia el input
@@ -19,13 +21,21 @@ function App() {
     const newSearch = e.target.value
     if (newSearch.startsWith(' ')) return
     setSearch(newSearch)
+    //getMovies(newSearch) => Buscar al cambiar el input
   }
 
+  const handleSortedMovies = () => {
+    setSort(!sort)
+  }
   
 
   return (
     <main className="flex flex-col justify-center items-center bg-gray-800 min-h-screen pt-8">
       <h1 className="text-2xl p-4 text-white font-semibold">Buscador de películas</h1>
+      <div className="flex gap-x-2">
+        <h2 className="text-white font-semibold">Ordenar películas A - Z</h2>
+        <input type="checkbox" onChange={handleSortedMovies}/>
+      </div>
       <form onSubmit={handleSubmit} className="flex gap-4 items-center">
         <input /* ref={inputRef} */ value={search} onChange={handleInputChange} name="search" className="rounded bg-slate-700 border-2 border-slate-800 select-none text-white outline-none m-2 p-1" type="text" placeholder="Avengers, Star wars..." />
         <button type="submit" className="px-4 text-xs h-8 rounded text-white bg-slate-600">Buscar</button>
--- src/hooks/getMovies.ts
+++ src/hooks/getMovies.ts
@@ -1,18 +1,21 @@
-import { useState } from "react"
+import { useCallback, useMemo, useRef, useState } from "react"
 import type { Movie } from "../interfaces/movies-interface"
 import type { MovieResponse } from "../interfaces/movie-response"
 
 interface Props {
     search: string
+    sort: boolean
 }
 
-export function useGetMovies({search}:Props) {
+export function useGetMovies({search, sort}:Props) {
     const [responseMovies, setResponseMovies] = useState<Movie[]>([])
     const [loading, setLoading] = useState(false)
-    const getMovies = async() => {
-        if(search) {
+    const previousSearch = useRef(search)
+    const getMovies = useCallback(async (search:string) => {
+        if(search === previousSearch.current) return // Evitar volver a buscar
             try {
                 setLoading(true)
+                previousSearch.current = search
                 const res = await fetch(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=${search}`)
                 const data = await res.json()
                 const { Search } = data 
@@ -24,15 +27,17 @@ export function useGetMovies({search}:Props) {
                     type: movie.Type
                 }))
                 if(!res.ok) throw new Error("Response error")
-                if(data) {
-                    setResponseMovies(mappedMovies)
-                    setLoading(false)
-                }
+                setResponseMovies(mappedMovies)
+                setLoading(false)
+                
             } catch (error) {
                 throw new Error("Error at fetching to https://www.odbapi.com")
             }
-        }
-    }
+    },[])
 
-    return { responseMovies, getMovies, loading }
+    const sortedMovies = useMemo(() => {
+        return sort ? [...responseMovies].sort((a, b) => a.title.localeCompare(b.title)) : responseMovies
+    }, [sort, responseMovies])
+
+    return { responseMovies: sortedMovies, getMovies, loading }
 }
